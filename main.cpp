0a714f1e087ba4cd4df176826d8424a84da733ca
antonieto <tony_monroe1212@hotmail.com>
2021-04-29 14:37:04 -0500

    Instancia de Cliente creada

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include <iostream> 
+#include <iostream>  
+#include "Cliente.h"
 
 using namespace std; 
 
 int main(){ 
-    
+    //Instancias de clases 
+    //Cliente  
+    Cliente clienteUno;
+
     
     return 0;
 }
