33fe2d5ee954c4ecea6ed5ef2aaedbb4ede24e35
antonieto <tony_monroe1212@hotmail.com>
2021-04-30 17:57:11 -0500

    .

--- Orden.h
+++ Orden.h
@@ -6,37 +6,6 @@ class Ordenes
 {
 //private:
 
-
-<<<<<<< HEAD
-class Orden{
-    private:
-        Producto* carro;
-        Empleado vendedor;
-        Cliente cliente;
-        
-
-    public:
-        Orden();
-        Orden(Producto*,Empleado,Cliente); 
-        void agregaAddProducto(Producto);
-};
-#endif 
-
-/* public:
-    int id;
-    Inventario inventarios;
-    Clientes cliente;
-    float total;
-    string formapago;
-    Ordenes();
-    Ordenes(int i, Inventario in, Clientes cl, float t, string fp);
-    void agregarProducto(string c, int cant, Inventario in_total);
-    void quitarProducto(string c);
-    void totalOrden();
-    void muestraDatos();   
-
-}; */
-=======
 public:
     int id;
     Inventario inventarios;
@@ -51,4 +20,3 @@ public:
     void muestraDatos();   
 
 };
->>>>>>> 858f9d9e153de144277aaacb9d1317c6496ac558
