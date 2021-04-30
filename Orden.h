141faf45877c2cfa6f2af093e5911694fa64f403
antonieto <tony_monroe1212@hotmail.com>
2021-04-30 17:42:12 -0500

    Comentarios

--- Orden.h
+++ Orden.h
@@ -10,13 +10,29 @@ using namespace std;
 
 class Orden{
     private:
-        Producto carro;
+        Producto* carro;
         Empleado vendedor;
         Cliente cliente;
         
 
     public:
         Orden();
-        Orden(Producto,Empleado,Cliente);
+        Orden(Producto*,Empleado,Cliente); 
+        void agregaAddProducto(Producto);
 };
-#endif
+#endif 
+
+/* public:
+    int id;
+    Inventario inventarios;
+    Clientes cliente;
+    float total;
+    string formapago;
+    Ordenes();
+    Ordenes(int i, Inventario in, Clientes cl, float t, string fp);
+    void agregarProducto(string c, int cant, Inventario in_total);
+    void quitarProducto(string c);
+    void totalOrden();
+    void muestraDatos();   
+
+}; */
