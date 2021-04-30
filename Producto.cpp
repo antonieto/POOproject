a296abf3c23960f4ae6d86f1bc3382eb066dd81e
Estefania <a01639270@itesm.mx>
2021-04-30 17:51:28 -0500

    ya

--- Producto.cpp
+++ Producto.cpp
@@ -1,1 +1,20 @@
-#include 
+#include <iostream>
+#include "Producto.h"
+using namespace std;
+Producto:: Producto(){
+    this->codigo="";
+    this->nombre="";
+    this->precioCompra=0.0;
+    this->precioVenta=0.0;
+    this->cantidadExistente=0;
+    this->peso=0.0;
+}
+
+Producto:: Producto(string codigo, string nombre,float precioCompra, float precioVenta,int cantidadExistente,float peso,float tamaño){
+    this->codigo=codigo;
+    this->nombre=nombre;
+    this->precioCompra=precioCompra;
+    this->precioVenta=precioVenta;
+    this->cantidadExistente=cantidadExistente;
+    this->peso=peso;
+}
